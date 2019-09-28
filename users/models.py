2dab7f0d434c328bcc52fa455888979a3384ebc6
cmkishores <cmkishores@gmail.com>
2019-09-28 11:42:20 +0530

    Add custom user model with field user_type

--- users/models.py
+++ users/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+class CustomUser(AbstractUser):
+
+	# Two types of users sign up.  According to type, posting and other privilages are granted.
+	TYPE = [
+			('seller'),
+			('buyer'),
+
+		]
+
+	# CharField takes a maximum length of 50 characters and is limited to choices defined in the above dictionary
+	user_type = models.CharField(max_length=50,choices=TYPE)
