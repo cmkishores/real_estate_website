ef5a97944ef9958fb9f63cd98d4b4007e5d2b7e0
cmkishores <cmkishores@gmail.com>
2019-09-28 11:09:04 +0530

    Add new app user  and make related changes to settings.py,  added url path to include users.urls in project level url

--- realestate/settings.py
+++ realestate/settings.py
@@ -37,6 +37,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    #3rd party apps
+
+    #user-created apps
+    'users.apps.UsersConfig',
 ]
 
 MIDDLEWARE = [
