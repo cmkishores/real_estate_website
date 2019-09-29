42807dc814db106148427f5e40d3d64667d63a44
cmkishores <cmkishores@gmail.com>
2019-09-29 23:33:21 +0530

    Added choice fields for models.py in articles app

--- articles/models.py
+++ articles/models.py
@@ -1,3 +1,30 @@
 from django.db import models
 
+class Article(models.Model):
+
+	# Choice fields for adverticement property type
+
+	TYPE = [
+			('FT','Flats'),
+			('VL','Villas'),
+			('LD','Land'),
+			('CX','Shopping Complex'),
+
+			]
+
+	CONDITION = [
+			('NW','NEW'),
+			('UD','Used'),
+			('UC','Upcoming Project'),
+
+		]
+	class Meta:
+		verbose_name = 'Article'
+
+
+	type = models.CharField(max_length=30,choices=TYPE)
+	condition = models.CharField(max_length=30,choices=CONDITION)
+	owner_name = models.CharField(max_length=40)
+
+
 # Create your models here.
