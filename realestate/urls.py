fcef249de6d07c0c3af483e7ef5c715cbad9d183
cmkishores <cmkishores@gmail.com>
2019-10-04 17:10:32 +0530

    Change the project level url file to correct error in allauth url settings

--- realestate/urls.py
+++ realestate/urls.py
@@ -20,5 +20,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('users/',include('users.urls')),
     path('', include('articles.urls')),
-    path(r'^accounts/', include('allauth.urls')),
+    path('accounts/', include('allauth.urls')),
 ]
