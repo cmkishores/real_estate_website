ff48873d43abd70fdf4a619e731fef32fe43767a
cmkishores <cmkishores@gmail.com>
2019-10-05 11:06:16 +0530

    Add an account page for logged in users to see their account and make changes / add review for seller type accounts

--- realestate/urls.py
+++ realestate/urls.py
@@ -17,6 +17,7 @@ from django.contrib import admin
 from django.conf import settings
 from django.urls import path,include
 from django.conf.urls.static import static
+from django.views.generic import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -24,4 +25,5 @@ urlpatterns = [
     path('users/',include('django.contrib.auth.urls')),
     path('', include('articles.urls')),
     path('accounts/', include('allauth.urls')),
+    path('/profile',TemplateView.as_view(template_name='account.html'),name='profile'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
