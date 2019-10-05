b725ad9bbec5301466aab56ed90171a6e8cdbd1a
cmkishores <cmkishores@gmail.com>
2019-10-05 10:25:52 +0530

    Add changes to display add article button only for sellers

--- users/views.py
+++ users/views.py
@@ -6,7 +6,7 @@ from django.urls import reverse_lazy
 
 class SignUpView(CreateView):
 	form_class = CustomUserCreationForm
-	success_url = reverse_lazy('login')
+	success_url = reverse_lazy('home')
 	template_name = 'signup.html'
 
 # Create your views here.
