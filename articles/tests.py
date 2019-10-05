5ce4fe935b9c2672b1b93f4feadb3bcbca7cac04
cmkishores <cmkishores@gmail.com>
2019-10-05 11:49:26 +0530

    write setup function for testing

--- articles/tests.py
+++ articles/tests.py
@@ -1,3 +1,12 @@
 from django.test import TestCase
 
+from .models import Article
+from django.urls import reverse, reverse_lazy
+
+class ArticleTest(TestCase):
+	def setup(self):
+		# Function to create dummy data for testing
+
+		self.post = Article.objects.create(category="Flats",condition="Used",owner_name="test",owner_address="test address",description="test description",location="test location",owner="cmk")
+
 # Create your tests here.
