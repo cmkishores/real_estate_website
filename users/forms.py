1169cefc57daefd13b2a10bffd92789ad58b6896
cmkishores <cmkishores@gmail.com>
2019-10-11 16:14:39 +0530

    Add custom form for user sign up inherited from allauth forms

--- users/forms.py
+++ users/forms.py
@@ -1,10 +1,11 @@
 from .models import CustomUser 
+from allauth.socialaccount.forms import SignupForm
 #imports the custom user created in users.models
 from django import forms
 from django.contrib.auth.forms import UserCreationForm, UserChangeForm
 
 
-class CustomUserCreationForm(UserCreationForm):
+class CustomUserCreationForm(SignupForm):
 
 	#This class creates a custom user creation form inheriting the built-in UserCreationForm and builts on top of that. 
 
