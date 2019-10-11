54b26edc943c8d6344ce80e63ea7a45984a27456
cmkishores <cmkishores@gmail.com>
2019-10-11 13:30:43 +0530

    New property added deleting the old ones

--- articles/urls.py
+++ articles/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
-from .views import AddArticle,EditArticle,DeleteArticle,ArticlesListView,ArticleDetailView
-
+from .views import AddArticle,EditArticle,DeleteArticle,ArticlesListView,ArticleDetailView,AddComment
 urlpatterns = [
 	path('',ArticlesListView.as_view(),name='home'),
 	path('property/<int:pk>/',ArticleDetailView.as_view(),name='article'),
 	path('property/new/', AddArticle.as_view(),name='addarticle'),
 	path('property/<int:pk>/edit',EditArticle.as_view(),name='editarticle'),
 	path('property/<int:pk>/delete/',DeleteArticle.as_view(),name='deletearticle'),
+	path('property/<int:pk>/comment/', AddComment.as_view(), name='add_comment_to_post'),
+
+
 ]
