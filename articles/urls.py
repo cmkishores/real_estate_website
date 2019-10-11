2f0af578613d8e9dd9ad620ef99f837521e13c17
cmkishores <cmkishores@gmail.com>
2019-10-11 09:57:52 +0530

    Change url structure of urls.py

--- articles/urls.py
+++ articles/urls.py
@@ -3,8 +3,8 @@ from .views import AddArticle,EditArticle,DeleteArticle,ArticlesListView,Article
 
 urlpatterns = [
 	path('',ArticlesListView.as_view(),name='home'),
-	path('<int:pk>/',ArticleDetailView.as_view(),name='article'),
-	path('new/', AddArticle.as_view(),name='addarticle'),
-	path('<int:pk>/edit',EditArticle.as_view(),name='editarticle'),
-	path('<int:pk>/delete/',DeleteArticle.as_view(),name='deletearticle'),
+	path('property/<int:pk>/',ArticleDetailView.as_view(),name='article'),
+	path('property/new/', AddArticle.as_view(),name='addarticle'),
+	path('property/<int:pk>/edit',EditArticle.as_view(),name='editarticle'),
+	path('property/<int:pk>/delete/',DeleteArticle.as_view(),name='deletearticle'),
 ]
