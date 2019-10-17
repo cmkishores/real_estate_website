1141c683ef39572e9f741cf01fe29937db66f504
cmkishores <cmkishores@gmail.com>
2019-10-17 16:03:48 +0530

    Make change to test.py - add a mock image and upload during setup

--- articles/tests.py
+++ articles/tests.py
@@ -8,33 +8,35 @@ class ArticleTest(TestCase):
 	def setup(self):
 		# Function to create dummy data for testing
 		# Test function other than listing is not working
-
-		self.post = Article.objects.create(category="Flats",condition="Used",owner_name="test",owner_address="test address",description="test description",location="test location",photo="")
+		photoMock = open(media/images/gnome.jpeg)
+		self.post = Article.objects.create(category="Flats",condition="Used",owner_name="test",owner_address="test address",description="test description",location="test location",photo=photoMock)
 
 	def test_article_content(self):
 		
-		pass
-		#article_object = Article.objects.get(pk=1)
-		#expected_object_cat = f'{article_object.category}'
-		#expected_object_cond = f'{article_object.condition}'
-		#self.assertEqual(expected_object_cat,'Flats')
-		#return self.assertEqual(expected_object_cond, 'Used')
+		
+		article_object = Article.objects.get(pk=1)
+		expected_object_cat = f'{article_object.category}'
+		expected_object_cond = f'{article_object.condition}'
+		self.assertEqual(expected_object_cat,'Flats')
+		return self.assertEqual(expected_object_cond, 'Used')
 
 	def test_list_view(self):
 		response = self.client.get(reverse('home'))
 		self.assertEqual(response.status_code, 200)
 
 		
-		#self.assertTemplateUsed(response, 'index.html')
+		self.assertTemplateUsed(response, 'index.html')
 
 	def test_article_details(self):
 		response = self.client.get('/1/')
-		#self.assertEqual(response.status_code, 200)
+		false_response = self.client.get('/100/')
+
+		self.assertEqual(response.status_code, 200)
+		self.assertEqual(false_response.status_code, 404)
 	
 
 	def test_articleedit_view(self):
 		response = self.client.post(reverse('editarticle', args='1'),{
 			'owner_name' : 'testedit',
 			})
-		#self.assertContains(response, 'testedit')
-		#self.assertEqual(response.status_code, 200)
+		self.assertContains(response, 'testedit')
